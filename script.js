7b6519d773fcda496d93a3661d17792dbab2beb8
jjozic <jjozic98@gmail.com>
2020-06-11 14:58:56 +0200

    Added possibility for player input
    - Player 1 plays with an 'X'
    - Player 2  plays with an 'O'

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-function drawBoard(){
-    var gameField = document.getElementById('gamefield')
-    
-    for(i = 0; i < 3; i++){
+const gameField = document.getElementById('gamefield')
+const player1 = document.getElementById('player1')
+const player2 = document.getElementById('player2')
+var currentPlayer = 1
+
+function drawBoard() {
+    for (i = 0; i < 3; i++) {
         let row = document.createElement('tr')
-        for(j = 0; j < 3; j++){
+        for (j = 0; j < 3; j++) {
             let col = document.createElement('td')
-            col.innerHTML = 'X'
-            col.addEventListener('click', () => col.innerHTML = 'O')
+            col.addEventListener('click', () => gameLogic(col))
             row.appendChild(col)
         }
         gameField.appendChild(row)
+    }
+}
+
+function gameLogic(col) {
+    if(col.innerHTML === ''){
+        if (currentPlayer === 1) {
+            player1.classList.remove('selected')
+            player2.classList.add('selected')
+            col.innerHTML = 'X'
+            currentPlayer = 2
+        }
+        else if (currentPlayer === 2){
+            player2.classList.remove('selected')
+            player1.classList.add('selected')
+            col.innerHTML = 'O'
+            currentPlayer = 1
+        }
     }
 }
 
